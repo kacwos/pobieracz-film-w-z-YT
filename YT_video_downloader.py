5d4a5ff2c30badb04c359389f7467d578e3f9e25
RUDY <148717195+kacwos@users.noreply.github.com>
2024-02-25 18:13:43 +0100

    Create YT_video_downloader.py

---
+++ YT_video_downloader.py
@@ -0,0 +1,35 @@
+import os
+from pytube import YouTube
+from tqdm import tqdm
+
+url = ""  # Wprowadź właściwy adres URL filmu
+video = YouTube(url)
+stream = video.streams.filter(only_audio=False).get_highest_resolution()
+
+# Ustal długość pliku
+file_size = stream.filesize
+
+# Utwórz katalog, jeśli nie istnieje
+output_directory = "C:\\Users\\Uzytkownik1\\PycharmProjects\\ytvideodownloader"
+os.makedirs(output_directory, exist_ok=True)
+
+# Usuń niedozwolone znaki ze tytułu
+cleaned_title = "".join(c for c in video.title if c.isalnum() or c in [' ', '.', '-'])
+
+# Utwórz pasek postępu
+progress_bar = tqdm(total=file_size, unit='B', unit_scale=True, unit_divisor=1024)
+
+# Pobieraj bloki i zaktualizuj pasek postępu
+def on_progress(chunk, file_handle, remaining):
+    if isinstance(chunk, bytes):
+        chunk = len(chunk)
+    progress_bar.update(chunk)
+
+# Ustaw funkcję postępu
+stream.on_progress = on_progress
+
+# Pobierz plik
+stream.download(output_path=output_directory, filename=f'{cleaned_title}.mp4')
+
+# Zamknij pasek postępu
+progress_bar.close()
