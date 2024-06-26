65d4d569755ad31363af81618d2d83e62bdcf13c
RUDY <148717195+kacwos@users.noreply.github.com>
2024-06-26 21:35:58 +0200

    Update YT_video_downloader.py

--- YT_video_downloader.py
+++ YT_video_downloader.py
@@ -1,35 +1,21 @@
-import os
 from pytube import YouTube
-from tqdm import tqdm
 
-url = ""  # Wprowadź właściwy adres URL filmu
-video = YouTube(url)
-stream = video.streams.filter(only_audio=False).get_highest_resolution()
+def download_highest_resolution_video(url, output_directory):
+    try:
+        video = YouTube(url)
 
-# Ustal długość pliku
-file_size = stream.filesize
+        stream = video.streams.get_highest_resolution()
 
-# Utwórz katalog, jeśli nie istnieje
-output_directory = ""
-os.makedirs(output_directory, exist_ok=True)
+        stream.download(output_path=output_directory)
 
-# Usuń niedozwolone znaki ze tytułu
-cleaned_title = "".join(c for c in video.title if c.isalnum() or c in [' ', '.', '-'])
+        print(f"Pobrano film w najwyższej rozdzielczości: {stream.title}")
 
-# Utwórz pasek postępu
-progress_bar = tqdm(total=file_size, unit='B', unit_scale=True, unit_divisor=1024)
+    except Exception as e:
+        print(f"Wystąpił błąd: {e}")
 
-# Pobieraj bloki i zaktualizuj pasek postępu
-def on_progress(chunk, file_handle, remaining):
-    if isinstance(chunk, bytes):
-        chunk = len(chunk)
-    progress_bar.update(chunk)
 
-# Ustaw funkcję postępu
-stream.on_progress = on_progress
+if __name__ == "__main__":
+    url = input("podaj url: ")
+    output_directory = "C:\\ściezka\\do\\miejsca\\zapisu"
 
-# Pobierz plik
-stream.download(output_path=output_directory, filename=f'{cleaned_title}.mp4')
-
-# Zamknij pasek postępu
-progress_bar.close()
+    download_highest_resolution_video(url, output_directory)
